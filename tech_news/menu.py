1cae5f9d0eceb7f8c2a8e93bcd61e65490b6e509
Jonathan Reis de Andrade <jonathanreisdeandrade@gmail.com>
2022-11-10 04:02:27 -0300

    feat: implementa funcionalidades do menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,4 +1,12 @@
 import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_tag,
+    search_by_category,
+)
 
 OPTIONS_LIST = (
     "Selecione uma das opções a seguir:\n"
@@ -14,36 +22,44 @@ OPTIONS_LIST = (
 
 
 def populate_database():
-    input("Digite quantas notícias serão buscadas: ")
-    print("populate_database")
+    amount_of_news = input("Digite quantas notícias serão buscadas: ")
+    print("\nPopulando o banco de dados...")
+    get_tech_news(int(amount_of_news))
+    print("Banco de dados populado")
 
 
 def search_news_by_title():
-    input("Digite o título: ")
-    print("search_news_by_title")
+    title = input("Digite o título: ")
+    news = search_by_title(title)
+    print("\n", news, sep="")
 
 
 def search_news_by_date():
-    input("Digite a data no formato aaaa-mm-dd: ")
-    print("search_news_by_date")
+    date = input("Digite a data no formato aaaa-mm-dd: ")
+    news = search_by_date(date)
+    print("\n", news, sep="")
 
 
 def search_news_by_tag():
-    input("Digite a tag: ")
-    print("search_news_by_tag")
+    tag = input("Digite a tag: ")
+    news = search_by_tag(tag)
+    print("\n", news, sep="")
 
 
 def search_news_by_category():
-    input("Digite a categoria: ")
-    print("search_news_by_category")
+    category = input("Digite a categoria: ")
+    news = search_by_category(category)
+    print("\n", news, sep="")
 
 
 def show_top_5_news():
-    print("show_top_5_news")
+    news = top_5_news()
+    print("\n", news, sep="")
 
 
 def show_top_5_categories():
-    print("show_top_5_categories")
+    categories = top_5_categories()
+    print("\n", categories, sep="")
 
 
 functions = {
@@ -72,3 +88,6 @@ def analyzer_menu():
     except KeyError:
         print("Opção inválida", file=sys.stderr)
         return 1
+    except Exception as error:
+        print(error, file=sys.stderr)
+        return 2
